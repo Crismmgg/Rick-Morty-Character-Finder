fb18a5e697a539c5abe1b3e2a096b0db3a0b0ca0
crismmgg <cristinammgg@gmail.com>
2020-01-13 18:57:34 +0100

    add icons

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../stylesheets/details.scss';
-import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRedditAlien } from '@fortawesome/free-brands-svg-icons';
+import { faUserCheck, faSkullCrossbones, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+
+
+import { Link } from 'react-router-dom';
+
 import PropTypes from 'prop-types';
 
 
 
 function CharacterDetails(props) {
   const { name, image, species, origin, episode, status } = props.characters
+  const isAlien = species === 'Alien' ? <FontAwesomeIcon icon={faRedditAlien} /> : <FontAwesomeIcon icon={faUserCheck} />
+  const isAlive = status === 'Alive' ? <FontAwesomeIcon icon={faThumbsUp} /> : <FontAwesomeIcon icon={faSkullCrossbones} />
   return (
     < ul >
       < li className='detail'>
@@ -14,13 +22,13 @@ function CharacterDetails(props) {
         <div className='detail__container'>
           <img className='detail__img' src={image} alt={name} />
           <div className='detail__info'>
-            <p className='detail__specie'>Especie: {species}</p>
+            <p className='detail__specie'>Especie: {species} {isAlien}</p>
             <p className='detail__planet'>Planeta de origen: {origin.name}</p>
             {/* {episode.map(caps => {
                 return <li>{caps}</li>
               })} */}
             <p className='detail__episodes'>Nº Episodios: {episode.length}</p>
-            <p className='detail__status'>Estado: {status}</p>
+            <p className='detail__status'>Estado: {status} {isAlive}</p>
           </div>
         </div>
         <Link to='/'>
